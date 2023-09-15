cd055ca18d25a44d4aa20e174120b3bc4593d438
Mateusz Pietrzak <mateusz.jerzy.pietrzak@gmail.com>
2023-09-15 18:00:13 +0200

    Assets again

--- src/header.asm
+++ src/header.asm
@@ -1,4 +1,5 @@
 INCLUDE "include/hardware.inc/hardware.inc"
+INCLUDE "utility.asm"
 
 SECTION "entry", ROM0[$100]
   jp EntryPoint 
@@ -71,80 +72,12 @@ MainLoop:
 skipRotationModulo:
     ld [_OAMRAM + 2], a
 
+    ; Check input
+    ld a, [wKeysPressed]
+    and $10
+    jp z, MainLoop
+    ld a, [_OAMRAM + 1]
+    inc a
+    ld [_OAMRAM+1], a
+
     jp MainLoop 
-
-Memcpy:
-    ld a, [de]
-    ld [hl+], a
-    inc de
-    dec bc
-    ld a, b
-    or a, c
-    jp nz, Memcpy
-    ret
-
-WaitForVBlank:
-    ld a, [rLY]
-    cp 144
-    jp c, WaitForVBlank
-    ret
-
-ClearOam:
-    xor a
-    ld b, $A0
-    ld hl, _OAMRAM
-.clearOamLoop:
-    ld [hl+], a
-    dec b
-    jp nz, ClearOam.clearOamLoop
-    ret
-
-UpdateKeys:
-    ld a, P1F_GET_BTN
-    ldh [rP1], a
-
-    call PollKeys
-
-    or a, $F0
-    ld b, a
-
-    ld a, P1F_GET_DPAD
-    ldh [rP1], a
-
-    call PollKeys
-
-    or a, $F0
-    swap a
-    xor a, b
-    
-
-    ld [wKeysPressed], a
-
-    ld a, P1F_GET_NONE
-    ldh [rP1], a
-
-    ret
-
-PollKeys:
-    ldh a, [rP1]
-    ldh a, [rP1]
-    ldh a, [rP1]
-    ldh a, [rP1]
-    ldh a, [rP1]
-    ldh a, [rP1]
-    ldh a, [rP1]
-    ldh a, [rP1]
-    ldh a, [rP1]
-    ldh a, [rP1]
-    ldh a, [rP1]
-    ret
-
-SECTION "graphics", ROM0
-graphicTiles:
-    incbin "assets/PlayerSprite.2bpp"
-.end:
-
-SECTION "variables", wram0
-
-wFrameCounter: db
-wKeysPressed: db
